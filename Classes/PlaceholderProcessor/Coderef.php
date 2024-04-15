cac22c1ca585a87ebf00adc271c7369bab3cfb27
Sascha Brümmer <ping@saschabruemmer.de>
2024-04-15 17:01:51 +0200

    [TASK] Migrate to TYPO3 v12
    Drop compatibility with outdated TYPO3 versions

--- Classes/PlaceholderProcessor/Coderef.php
+++ Classes/PlaceholderProcessor/Coderef.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SomeBdyElse\Coderef\PlaceholderProcessor;
 
+use Doctrine\DBAL\Exception\TableNotFoundException;
 use TYPO3\CMS\Core\Configuration\Processor\Placeholder\PlaceholderProcessorInterface;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -19,14 +20,18 @@ class Coderef implements PlaceholderProcessorInterface
     {
         [$table, $coderef] = explode(':', $value);
 
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
-        $uid = $queryBuilder
-            ->select('uid')
-            ->from($table)
-            ->where(
-                $queryBuilder->expr()->eq('tx_coderef_identifier', $queryBuilder->createNamedParameter($coderef))
-            )
-            ->execute()->fetchOne();
+        try {
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
+            $uid = $queryBuilder
+                ->select('uid')
+                ->from($table)
+                ->where(
+                    $queryBuilder->expr()->eq('tx_coderef_identifier', $queryBuilder->createNamedParameter($coderef))
+                )
+                ->execute()->fetchOne();
+        } catch (TableNotFoundException $tableNotFoundException) {
+            throw new \UnexpectedValueException('Codref table not found: ' . $table, 1713194130145);
+        }
 
         if ($uid === false) {
             throw new \UnexpectedValueException('Coderef not found: ' . $value, 1621033619105);
